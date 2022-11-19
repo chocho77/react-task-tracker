42ced1b57bc7c47db121ae2405830f4fb68fff01
User <user@Users-MacBook.local>
2022-11-19 23:07:16 +0200

    Changes to be committed: 	modified:   src/App.js

--- src/App.js
+++ src/App.js
@@ -2,10 +2,11 @@
 //import './App.css';
 
 function App() {
+  const name = 'Chavdar';
   return (
     <div className="App">
       <h1>Hello From React</h1>
-      <h2>Hello</h2>
+      <h2>Hello {name}</h2>
     </div>
   );
 }
