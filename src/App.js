c1e648d50665d8813d2bbf2e0722c8ba1a0484f2
User <user@Users-MacBook.local>
2022-11-19 23:40:23 +0200

    Committer: Chavdar <user@Users-MacBook.local>  On branch master  Your branch is up-to-date with 'origin/master'.  Changes to be committed: 	modified:   src/App.js 	new file:   src/components/Header.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,11 @@
-//import logo from './logo.svg';
 //import './App.css';
+import Header from './components/Header';
 
 function App() {
-  const name = 'Chavdar';
-  const x = true;
+ 
   return (
     <div className="App">
-      <h1>Hello From React</h1>
-      <h2>Hello {name}</h2>
-      <h3>{x ? 'Yes' : 'No' }</h3>
+      <Header />
     </div>
   );
 }
