1a9780edf38ab61a4f8e759e48051757bfb2cb53
User <user@Users-MacBook.local>
2022-11-19 23:13:35 +0200

    Committer: Chavdar <user@Users-MacBook.local>  On branch master  Your branch is up-to-date with 'origin/master'.  Changes to be committed: 	modified:   src/App.js

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@
 
 function App() {
   const name = 'Chavdar';
+  const x = true;
   return (
     <div className="App">
       <h1>Hello From React</h1>
       <h2>Hello {name}</h2>
+      <h3>{x ? 'Yes' : 'No' }</h3>
     </div>
   );
 }
