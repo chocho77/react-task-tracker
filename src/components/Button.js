8670b7f37b9816a030cc0e6a1e3f00796621eff9
User <user@Users-MacBook.local>
2022-11-22 14:18:20 +0200

    Committer: Chavdar <chavdar@Users-MacBook.local>  On branch master  Your branch is up-to-date with 'origin/master'.  Changes to be committed: 	new file:   src/components/Button.js 	modified:   src/components/Header.js

---
+++ src/components/Button.js
@@ -0,0 +1,29 @@
+import PropTypes from 'prop-types';
+
+
+const Button = ({ color, text }) => {
+    const onClick = () => {
+        console.log('click');
+    }
+    
+
+    return  (
+    <button 
+    onClick={onClick}
+    style={{ backgroundColor: color }}
+     className='btn'>
+        {text}
+    </button>
+    )   
+}
+
+Button.defaultProps = {
+    color: 'steelblue',
+}
+
+Button.psropTypes = {
+    text: PropTypes.string,
+    color: PropTypes.string,
+}
+
+export default Button
