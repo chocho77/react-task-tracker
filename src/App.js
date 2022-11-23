96002e6519ca698b3280cec05426f11953981f4b
User <user@Users-MacBook.local>
2022-11-23 10:51:32 +0200

    Committer: Chavdar <chavdar@Users-MacBook.local>  On branch master  Your branch is up-to-date with 'origin/master'.  Changes to be committed: 	modified:   src/App.js 	modified:   src/components/Tasks.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 //import './App.css';
 import Header from './components/Header';
+import Tasks from './components/Tasks';
 
  const App = () => {
     return (
    <div className="container">
        <Header />
+       <Tasks />
     </div>       
 );
 }
--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,7 +1,29 @@
+const tasks = [
+    {
+        id: 1,
+        text: 'Doctors Appointment',
+        day: 'Feb 5th at 2:30 pm',
+        remainder: true,
+    },
+    {
+        id: 2,
+        text: 'Meeting at School',
+        day: 'Feb 6th at 1:30 pm',
+        reminder:true,
+    },
+    {
+        id: 3,
+        text: 'Food Shopping',
+        day: 'Feb 5th at 2:30 pm',
+        reminder:false,
+    }
+];
+
 const Tasks = () => {
     return (
         <div>
-
+          {tasks.map((task) => (<h3>{task.text}</h3>)
+          )}
         </div>
     )
 }
