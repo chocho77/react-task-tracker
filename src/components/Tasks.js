34f80735ad5a446eb8a232f734701b856cf032f6
User <user@Users-MacBook.local>
2022-11-23 13:51:30 +0200

    Committer: User <user@Users-MacBook.local>  On branch master  Your branch is up-to-date with 'origin/master'.  Changes to be committed: 	copied:     src/components/Tasks.js -> src/App.js 	modified:   src/components/Tasks.js

--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,27 +1,5 @@
-import { useState } from 'react';
-
-const Tasks = () => {
-    const [tasks, setTasks] = useState([
-        {
-            id: 1,
-            text: 'Doctors Appointment',
-            day: 'Feb 5th at 2:30 pm',
-            remainder: true,
-        },
-        {
-            id: 2,
-            text: 'Meeting at School',
-            day: 'Feb 6th at 1:30 pm',
-            reminder:true,
-        },
-        {
-            id: 3,
-            text: 'Food Shopping',
-            day: 'Feb 5th at 2:30 pm',
-            reminder:false,
-        }]
-    );
-    
+const Tasks = ({ tasks }) => {
+   
     return (
         <div>
           {tasks.map((task) => (<h3 key={task.id}>{task.text}</h3>)
