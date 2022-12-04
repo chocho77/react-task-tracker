1f3ed68e28f3a9e1be4d6b40abccab56478c8871
User <user@Users-MacBook.local>
2022-12-04 06:08:44 +0200

    Committer: Chavdar <chavdar@Users-MacBook.local>  On branch master  Your branch is up-to-date with 'origin/master'.  Changes to be committed: 	modified:   src/components/Task.js

--- src/components/Task.js
+++ src/components/Task.js
@@ -3,7 +3,7 @@ import { FaTimes } from 'react-icons/fa';
 const Task = ({ task, onDelete, onToggle }) => {
     return (
         <div className='task' onDoubleClick={() =>
-        onToggle(id)}>
+        onToggle(task.id)}>
             <h3>
                 {task.text} <FaTimes style={{color:'red', 
                 cursor:'pointer' }}
